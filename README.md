53c8dfa0182c8d3672499cdedac23d4aafd8da7c
RealShiftyDev <sneaxttv@gmail.com>
2019-08-24 23:20:28 +0000

    the first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bionic Discord Bot
+
+Bionic Is A Discord Bot Maintained By Shifty#1010 with the help of menudocs( https://discord.gg/ePCj63c ) which is a nice coding community!
+
+## Installation
+
+A Quick Guide To Installing Bionic Bot!
+
+**#1** *-* Download Git
+**#2** *-* Clone The Repo
+**#3** *-* Edit as much as you want!
+
+### Usage
+
+The Default Prefix For The Bot Is **!** you can do !help to get all commands
+
+#### Contributing
+
+You wanna help me build up commands? Feel Free To Do So By Sending Us A Pull Request With your New Code!
+
+#### License
+
+As This Is Open Source, You May Fork This Repo But You Still Have To Credit Me :)
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
---
+++ index.js
@@ -0,0 +1,38 @@
+const botconfig = require('./botconfig.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", async () => {
+    console.log(`${client.user.username} is now Active!`);
+    client.user.setActivity('With Discord!');
+})
+
+const fs = require('fs');
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.log(err);
+
+    const jsfile = files.filter(f => f.endsWith('.js'));
+    if (jsfile.length <= 0) return console.log("[FS] Couldn't Find Commands!");
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        client.commands.set(pull.config.name, Object.assign(pull, {
+            triggers: [ pull.config.name, ...(pull.config.aliases || []) ]
+        }))
+    })
+});
+
+client.on("message", async message => {
+    const prefix = botconfig.prefix;
+    if(message.author.bot || message.channel.type === 'dm') return;
+    if(!message.content.startsWith(prefix)) return;
+
+    const [ cmd, ...args ] = message.content.slice(prefix.length).split(/ +/g)
+
+    let commandFile = client.commands.find(c => c.triggers.includes(cmd.toLowerCase()));
+    if(commandFile) commandFile.run(client, message, args)
+})
+
+client.login(botconfig.token)
